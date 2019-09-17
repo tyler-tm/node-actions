0cec642369768a3182a50ce13a5ac0f8552af954
Tyler M <tyler-tm@protonmail.com>
2019-09-16 20:26:00 -0400

    Break test

--- src/greeter.test.ts
+++ src/greeter.test.ts
@@ -3,7 +3,7 @@ import { greet } from './greeter';
 describe('greet', () => {
   it('should return a greeting with the name passed', () => {
     const name = 'Tyler';
-    const expected = 'Hello, Tyler!';
+    const expected = 'Hello, Frank!';
     expect(greet(name)).toEqual(expected);
   });
 });
