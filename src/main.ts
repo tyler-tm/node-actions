1ecf7fa9fb690f7754f4efb0aa238ed99ea26c81
Tyler M <tyler-tm@protonmail.com>
2019-09-16 20:30:59 -0400

    Done testing Actions, makes lint pass again

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { greet } from './greeter';
 
 const app = express();
 
-app.get("/:name", (req, res) => res.send(greet(req.params.name)));
+app.get('/:name', (req, res) => res.send(greet(req.params.name)));
 
 const port = 3000 || process.env.PORT;
 app.listen(port, () => console.log(`app listening on port ${port}!`));
